f3f5bb73ea7ac1e6883dbf8cc84897dea0ef832e
romaryo <you@example.com>
2023-01-14 23:45:09 -0800

    E commerce commit

---
+++ lib/shared/colors.dart
@@ -0,0 +1,11 @@
+
+
+// ignore_for_file: constant_identifier_names
+
+import 'package:flutter/material.dart';
+
+ const BTNpink = Color.fromARGB(255, 203, 90, 14);
+//  const BTNgreen = Color.fromARGB(255, 73, 179, 105);
+const BTNgreen = Color.fromARGB(255, 203, 90, 14);
+//  const appbarGreen = Color.fromARGB(255, 76, 141, 95);
+  const appbarGreen = Color.fromARGB(255, 203, 90, 14);
